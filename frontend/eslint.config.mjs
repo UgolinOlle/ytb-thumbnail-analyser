a17570316b28fccfb4471d46e6d57eb4cde60ed1
Ugolin Ollé <hello@ugolin-olle.com>
2024-10-04 17:09:56 +0700

    refactor(core): switch to mjs config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,49 +1,40 @@
-{
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "next/core-web-vitals",
-    "plugin:tailwindcss/recommended",
-    "prettier"
-  ],
-  "plugins": [
-    "@typescript-eslint",
-    "react",
-    "react-hooks",
-    "jsx-a11y",
-    "tailwindcss",
-    "prettier"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+import { fixupConfigRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import tailwind from 'eslint-plugin-tailwindcss';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+const compat = new FlatCompat();
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tailwind.configs['flat/recommended'],
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    plugins: {
+      'react-hooks': hooksPlugin,
     },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+    rules: hooksPlugin.configs.recommended.rules,
   },
-  "rules": {
-    "prettier/prettier": ["error"],
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { "argsIgnorePattern": "^_" }
-    ],
-    "react/react-in-jsx-scope": "off",
-    "tailwindcss/classnames-order": "warn",
-    "tailwindcss/no-custom-classname": "off"
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
+    },
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
+  {
+    ignores: ['tailwind.config.ts', '*.js'],
+  },
+];
